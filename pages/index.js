724fa9b2ac4ea01b2d58abac340988a5ed2d7dbe
anishmhrzn47 <anishamhrzn47@gmail.com>
2021-10-04 16:35:10 +1100

    create recipe website using Next.js and contentful

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,43 @@
-export default function Recipes() {
+//to connect to contentful
+import { createClient } from 'contentful';
+import RecipeCard from '../components/RecipeCard';
+
+//"getStaticProps" grab data and inject as props to display in browser
+export async function getStaticProps(){
+
+  //make connection to contentful space
+  const clients = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
+  //function to get items from content space
+  //content_type specifies which content to grab through the id
+  const res = await clients.getEntries({ content_type: 'recipe'})
+  return{
+    props: {
+      recipes: res.items
+    }
+  }
+
+}
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
   return (
     <div className="recipe-list">
-      Recipe List
+      {recipes.map(recipe => (
+        <RecipeCard key={recipe.sys.id} recipe = {recipe}  />
+      ))}
+
+      <style jsx> {`
+        .recipe-list{
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 6px;
+        }
+      `}</style>
+
     </div>
   )
 }
+
