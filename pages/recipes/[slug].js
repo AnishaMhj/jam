baae5bfa35dd544237d2ce8696fa9c4c1adf1afa
anishmhrzn47 <anishamhrzn47@gmail.com>
2021-10-13 19:51:11 +1100

    fallback

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -2,7 +2,7 @@
 import { createClient } from 'contentful';
 import Image from "next/image";
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-// import Skeleton from '../../components/Skeleton';
+import Skeleton from '../../components/Skeleton';
 
 //find paths or routes of the specific pages generated
 
